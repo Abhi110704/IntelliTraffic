ac367b3a28017b12960e27f0f27e265646f19ece
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-05-05 17:22:00 +0530

    Delete bas.py

--- bas.py
+++
@@ -1,8 +0,0 @@
-mkdir intelli_traffic
-cd intelli_traffic
-
-mkdir static templates uploads
-
-touch app.py yolov8_utils.py traffic_logic.py requirements.txt
-touch static/style.css static/script.js
-touch templates/index.html
